c1293900495986f3940ff4e5f8abc07a9b333d42
kazukazu2027 <kazu2000027@gmail.com>
2021-07-24 12:35:45 +0900

    スタンプボタン

--- src/resources/ts/component/Stamp.tsx
+++ src/resources/ts/component/Stamp.tsx
@@ -52,6 +52,12 @@ const Stamp = () => {
                         <div className="text-center">
                             <button
                                 onClick={openMordal}
+                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded my-5 mr-3"
+                            >
+                                スタンプ押す
+                            </button>
+                            <button
+                                onClick={openMordal}
                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-5"
                             >
                                 閉じる
