65e241504ab4bf1a819697dc38ddb42043f34854
himekuri <himekuri9@gmail.com>
2021-07-24 16:29:08 +0900

    api.phpとweb.phpの修正

--- src/routes/api.php
+++ src/routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('/punchin', 'StampsController@punchIn');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
--- src/routes/web.php
+++ src/routes/web.php
@@ -10,11 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+/*
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm');
 Route::post('signup', 'Auth\RegisterController@register');
@@ -24,14 +20,7 @@ Route::get('login', 'Auth\LoginController@showLoginForm');
 Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout');
 
-// スタンプ機能
-Route::group(['middleware' => 'auth'], function() {
-    Route::post('/punchin', 'StampsController@punchIn');
-});
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
+*/
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any','.*');
