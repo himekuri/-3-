6dd61d48f27060782b0b38ee6c0449b31c752456
kazukazu2027 <kazu2000027@gmail.com>
2021-07-24 17:17:43 +0900

    おみくじ

--- src/resources/ts/component/Fortune.tsx
+++ src/resources/ts/component/Fortune.tsx
@@ -2,20 +2,51 @@ import React, { useState, useEffect } from "react";
 import Card from "../parts/Card";
 import Overlay from "../Layouts/Overlay";
 import SlideIn from "../Layouts/SlideIn";
+import ReactCardFlip from "react-card-flip";
 
 const fortuneResult = ["大吉", "中吉", "吉", "凶"];
+const luckyItem = [
+    "鉛筆",
+    "ペットボトル",
+    "ノート",
+    "携帯",
+    "イヤホン",
+    "消しゴム",
+    "マッチ",
+    "財布"
+];
+const luckyColor = [
+    "ブルー",
+    "ブラック",
+    "レッド",
+    "イエロー",
+    "グリーン",
+    "ホワイト"
+];
 
-
-
-const Fortune: React.FC = () => {
+const Fortune: React.FC = ({ closeNav }: any) => {
     const [display, setDisplay] = useState(false);
     const [fortune, setFortune] = useState("");
     const [comment, setComment] = useState("");
+    const [color, setColor] = useState("");
+    const [item, setItem] = useState("");
+
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    function flipCard() {
+        setIsFlipped(!isFlipped);
+    }
 
     const todoFortune = () => {
         const fortune =
             fortuneResult[Math.floor(Math.random() * fortuneResult.length)];
         setFortune(fortune);
+
+        const item = luckyItem[Math.floor(Math.random() * luckyItem.length)];
+        setItem(item);
+
+        const color = luckyColor[Math.floor(Math.random() * luckyColor.length)];
+        setColor(color);
 
         setDisplay(!display);
         if (fortune === "大吉") {
@@ -42,13 +73,36 @@ const Fortune: React.FC = () => {
                 <div>
                     <Overlay>
                         <SlideIn>
-                            <p className="py-9">今日の運勢は{fortune}です</p>
-                            <p className="">{comment}</p>
+                            <div className="container" onClick={closeNav}>
+                                <ReactCardFlip isFlipped={isFlipped}>
+                                    <div className="" onClick={flipCard}>
+                                        <div className="flex flex-col">
+                                            <span>お</span>
+                                            <span>み</span>
+                                            <span>く</span>
+                                            <span>じ</span>
+                                        </div>
+                                    </div>
+                                    <div onClick={flipCard}>
+                                        <p className="">
+                                            今日の運勢は{fortune}です
+                                        </p>
+                                        <p className="">
+                                            ラッキーアイテムは{item}
+                                        </p>
+                                        <p className="">
+                                            ラッキーカラーは{color}
+                                        </p>
+                                        <p className="">{comment}</p>
+                                    </div>
+                                </ReactCardFlip>
+                            </div>
                             <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="button"
                                 onClick={closeMordal}
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-5"
                             >
-                                閉じる
+                                戻る
                             </button>
                         </SlideIn>
                     </Overlay>
