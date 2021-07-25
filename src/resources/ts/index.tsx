7f198e28a099ffa8c3511ffd1cb3bbb22f6c0c7c
kazukazu2027 <kazu2000027@gmail.com>
2021-07-25 12:46:38 +0900

    修正

--- src/resources/ts/index.tsx
+++ src/resources/ts/index.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TopPage from "./Page/TopPage";
-import LogInPage from "./Page/LogInPage";
 import TodoListPage from "./Page/TodoListPage";
-import MainPage from "./Page/MainPage";
-import SignUpPage from "./Page/SignUpPage";
 import "tailwindcss/tailwind.css";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/" exact component={MainPage} />
-                <Route path="/SignUpPage" exact component={SignUpPage} />
-                <Route path="/topPage" exact component={TopPage} />
-                <Route path="/LogInPage" component={LogInPage} />
+                <Route path="/" exact component={TopPage} />
                 <Route path="/todoListPage" component={TodoListPage} />
             </Switch>
         </Router>
